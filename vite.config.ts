04d3e75633600ecfec5d95cc45d49b785ad57a96
MistrAnderson <noteo.gaming@protonmail.com>
2025-03-24 23:38:02 +0100

    feat (vite): allow for custom import path, no need for relative path imports now

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
-import { VitePWA } from 'vite-plugin-pwa';
+import { VitePWA } from 'vite-plugin-pwa'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': '/src',
+      '@components': '/src/components'
+    }
+  },
   plugins: [react(), VitePWA({
     registerType: 'prompt',
     injectRegister: false,
