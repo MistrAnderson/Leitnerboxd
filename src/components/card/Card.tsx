9f18a70d02854cce32e9f368338f0c061dbcaf51
MistrAnderson <noteo.gaming@protonmail.com>
2025-03-26 19:58:57 +0100

    feat (quiz): make questions not show answer directly and add button to change state of card form isShowingAnswer = true to false

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,12 +1,26 @@
 import { ICard } from "@/idb/interfaces";
 import style from "./card.module.css"
+import { useState } from "react";
 
 // TODO: do the css of the components
 export default function Card( { cardInfo } : { cardInfo: ICard }) {
+  const [isShowingAnswer, setIsShowingAnswer] = useState(false)
+
+  const switchShowAnswer = () => {
+    setIsShowingAnswer(! isShowingAnswer)
+  }
+
   return (
     <div className={style.card}>
+
       <p>Question: {cardInfo.question}</p>
-      <p>Answer: {cardInfo.answer}</p>
+      { isShowingAnswer &&
+        <p>Answer: {cardInfo.answer}</p>
+      }
+
+      <button onClick={switchShowAnswer}>
+        { ! isShowingAnswer ? "Show" : "Hide" } answer
+      </button>
     </div>
   )
 }
