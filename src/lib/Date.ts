6efa024a36e313e785e05a82658a9d73bea1f335
MistrAnderson <noteo.gaming@protonmail.com>
2025-03-29 19:48:12 +0100

    refactor (lib): made lib directory to keep things clean

---
+++ src/lib/Date.ts
@@ -0,0 +1,33 @@
+import { ICard } from "@/idb/interfaces"
+
+export function getTodayCards(cards: ICard[]): ICard[] {
+  const newCardsList: ICard[] = []
+
+  console.log(cards)
+
+  cards.map((card) => {
+    const nextEncounter = getCalculateNextEncounter(new Date(card.previousEncounter), card.level)
+    console.log(nextEncounter)
+    console.log(formatDate(nextEncounter))
+    console.log(formatDate(new Date()))
+    console.log(formatDate(nextEncounter) <= formatDate(new Date()))
+
+    if (formatDate(nextEncounter) <= formatDate(new Date())) {
+      newCardsList.push(card)
+    }
+  })
+
+  return newCardsList
+}
+
+export function formatDate(date: Date): string {
+  return date.toISOString().split("T")[0]
+}
+
+export function getCalculateNextEncounter(prevEncounter: Date, level: number): Date {
+  const nextEncounter = new Date(prevEncounter)
+  nextEncounter.setDate(nextEncounter.getDate() + 2 ** (level - 1))
+
+  return nextEncounter
+}
+
