c8bdf0879eeb498908118d4f56704e0099194cc2
MistrAnderson <noteo.gaming@protonmail.com>
2025-03-29 19:09:25 +0100

    fix: card data structure since 5e05c3bf0b5644468b7afa645c4944e430dea20c

--- src/components/cards/Cards.tsx
+++ src/components/cards/Cards.tsx
@@ -11,11 +11,11 @@ export default function Cards() {
 
   console.log(cards)
 
-  const card1: ICard = { name: "card1", question: "q1", answer: "a1", level: 1, previousEncounter: "2025-02-18" }
-  const card2: ICard = { name: "card2", question: "q2", answer: "a2", level: 1, previousEncounter: "2025-02-18" }
-  const card3: ICard = { name: "card3", question: "q3", answer: "a3", level: 1, previousEncounter: "2025-02-18" }
-  const card4: ICard = { name: "card4", question: "q4", answer: "a4", level: 1, previousEncounter: "2025-02-18" }
-  const card5: ICard = { name: "card5", question: "q5", answer: "a5", level: 1, previousEncounter: "2025-02-18" }
+  const card1: ICard = {id: crypto.randomUUID(), name: "card1", question: "q1", answer: "a1", level: 1, previousEncounter: new Date("2025-02-18") }
+  const card2: ICard = {id: crypto.randomUUID(), name: "card2", question: "q2", answer: "a2", level: 1, previousEncounter: new Date("2025-02-18") }
+  const card3: ICard = {id: crypto.randomUUID(), name: "card3", question: "q3", answer: "a3", level: 1, previousEncounter: new Date("2025-02-18") }
+  const card4: ICard = {id: crypto.randomUUID(), name: "card4", question: "q4", answer: "a4", level: 1, previousEncounter: new Date("2025-02-18") }
+  const card5: ICard = {id: crypto.randomUUID(), name: "card5", question: "q5", answer: "a5", level: 1, previousEncounter: new Date("2025-02-18") }
 
   return (
     <div>
