92af52e938da9cd3c1e793f200949d3678ac5e21
MistrAnderson <noteo.gaming@protonmail.com>
2025-03-30 15:50:07 +0200

    feat/fix (quiz): Prints a message if no more cards are available to be quized on. And fix wrong iteration over cards

--- src/components/quiz/Quiz.tsx
+++ src/components/quiz/Quiz.tsx
@@ -2,19 +2,19 @@ import { ICard } from "@/idb/interfaces"
 import Card from '@components/card/Card'
 import { useAppDispatch, useAppSelector } from "@/store/hooks"
 import { updateCard } from "@/store/cardsData/cardsSlice"
-import { useState } from "react"
+import { useMemo, useState } from "react"
 import { getTodayCards } from "@/lib/Date"
 
 export default function Quiz() {
   const cards: ICard[] = useAppSelector(state => state.cards.cards)
   const dispatch = useAppDispatch()
   const [cardIndex, setCardIndex] = useState(0)
-  const todayCards = getTodayCards(cards)
+  const todayCards = useMemo(() => getTodayCards(cards), [])
 
-  console.log(todayCards)
+  const allCardsWereSeen = cardIndex >= todayCards.length
 
   const handleCardKnowledge = (isKnow: boolean) => {
-    if (cardIndex > todayCards.length - 1) return
+    if (cardIndex >= todayCards.length) return
 
     setCardIndex(cardIndex + 1)
 
@@ -31,12 +31,19 @@ export default function Quiz() {
 
   return (
     <div>
-      <Card key={todayCards[cardIndex].id} cardInfo={todayCards[cardIndex]} />
+      {allCardsWereSeen ? (
+        <div>No more cards for today !</div>
+      ) : (
+        <>
+          <Card key={todayCards[cardIndex].id} cardInfo={todayCards[cardIndex]} />
 
-      <div>
-        <button onClick={() => handleCardKnowledge(false)}>Wrong</button>
-        <button onClick={() => handleCardKnowledge(true)}>Correct</button>
-      </div>
+          <div>
+            <button onClick={() => handleCardKnowledge(false)}>Wrong</button>
+            <button onClick={() => handleCardKnowledge(true)}>Correct</button>
+          </div>
+        </>
+      )
+      }
     </div>
   )
 }
