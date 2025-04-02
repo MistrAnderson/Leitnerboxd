1c73d31fa9a46023ebc2be6a6aa98b827c139c61
MistrAnderson <noteo.gaming@protonmail.com>
2025-04-02 18:47:57 +0200

    fix: make button more modular with possibility to pass additionnal css to the component

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,18 +1,18 @@
 import style from './button.module.css'
 
-export default function Button({ children, type = "main", onClick, ...props}) {
+export default function Button({ children, type = "main", onClick, className, ...props}) {
   return(
-    <button className={`${style.button} ${style[type]}`} onClick={onClick} {...props}>
+    <button className={`${style.button} ${style[type]} ${className}`} onClick={onClick} {...props}>
       {children}
     </button>
   )
 }
 
-export function WrongButton({onClick, ...props }) {
-  return <Button type='wrong' onClick={onClick} {...props} />
+export function WrongButton({onClick, className, ...props }) {
+  return <Button type='wrong' onClick={onClick} className={className} {...props} />
 }
 
-export function CorrectButton({onClick, ...props }) {
-  return <Button type='correct' onClick={onClick} {...props} />
+export function CorrectButton({onClick, className, ...props }) {
+  return <Button type='correct' onClick={onClick} className={className} {...props} />
 }
 
