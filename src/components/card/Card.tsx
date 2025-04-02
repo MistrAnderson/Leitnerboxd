a492807ad5a4185d2b46dfacd8e0812d2a8d47ad
MistrAnderson <noteo.gaming@protonmail.com>
2025-04-02 19:44:53 +0200

    refactor: change component structure for better css

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,22 +1,35 @@
 import { ICard } from "@/idb/interfaces";
 import style from "./card.module.css"
 import { useState } from "react";
+import { formatDate, getCalculateNextEncounter } from "@/lib/Date";
 
-// TODO: do the css of the components
 export default function Card( { cardInfo } : { cardInfo: ICard }) {
   const [isShowingAnswer, setIsShowingAnswer] = useState(false)
+
+  const prevEncounter: Date = new Date(cardInfo.previousEncounter)
+  const nextEncounter: Date = getCalculateNextEncounter(prevEncounter, cardInfo.level)
+  const formatedNextEncounter: string = formatDate(nextEncounter)
 
   const switchShowAnswer = () => {
     setIsShowingAnswer(! isShowingAnswer)
   }
 
   return (
-    <div className={style.card}>
+    <div className={`${style.card} ${isShowingAnswer ? style.active : ""}`}>
 
-      <p>Question: {cardInfo.question}</p>
-      { isShowingAnswer &&
-        <p>Answer: {cardInfo.answer}</p>
-      }
+      <div className={style.cardRow}>
+        <span>{ isShowingAnswer ? "Answer" : "Question" }</span>
+        <span>Nv: {cardInfo.level}</span>
+      </div>
+
+      <div className={style.cardRow}>
+        <p>{ isShowingAnswer ? cardInfo.answer : cardInfo.question }</p>
+      </div>
+
+      <div className={style.cardRow}>
+        <span>{cardInfo.previousEncounter}</span>
+        <span>{formatedNextEncounter}</span>
+      </div>
 
       <button onClick={switchShowAnswer}>
         { ! isShowingAnswer ? "Show" : "Hide" } answer
