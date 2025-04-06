0a67e68b9e63361254785bd08326072b8fac1d0f
MistrAnderson <noteo.gaming@protonmail.com>
2025-04-06 14:26:48 +0200

    refactor: change prop name, no more overide of 'type' and reset button css before applying new one

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,18 +1,18 @@
 import style from './button.module.css'
 
-export default function Button({ children, type = "main", onClick, className, ...props}) {
+export default function Button({ children, contentType = "main", onClick, className, ...props}) {
   return(
-    <button className={`${style.button} ${style[type]} ${className}`} onClick={onClick} {...props}>
+    <button className={`${style.button} ${style[contentType]} ${className}`} onClick={onClick} {...props}>
       {children}
     </button>
   )
 }
 
 export function WrongButton({onClick, className, ...props }) {
-  return <Button type='wrong' onClick={onClick} className={className} {...props} />
+  return <Button contentType='wrong' onClick={onClick} className={className} {...props} />
 }
 
 export function CorrectButton({onClick, className, ...props }) {
-  return <Button type='correct' onClick={onClick} className={className} {...props} />
+  return <Button contentType='correct' onClick={onClick} className={className} {...props} />
 }
 
